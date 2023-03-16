f0f487177821aa97a44eb124c7562a55a6acef48
Ben Siraphob <bensiraphob@gmail.com>
2023-03-16 10:52:41 -0700

    New args, send message as file if too long

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ token = os.environ['DISCORD_TOKEN']
 
 @bot.event
 async def on_ready():
-    print('Logged in as {0.user}'.format(bot))
+    print(f'Logged in as {bot.user}')
 
 @bot.event
 async def on_message(message):
@@ -28,10 +28,11 @@ async def on_message(message):
             parser.add_argument('-p', '--prompt', type=str, required=True, help='prompt to start generation with')
             parser.add_argument('-c', '--ctx_size', type=int, default=512, help='size of the prompt context')
             parser.add_argument('-k', '--top_k', type=int, default=40, help='top-k sampling')
-            parser.add_argument('--top_p', type=float, default=0.9, help='top-p sampling')
+            parser.add_argument('--top_p', type=float, default=0.5, help='top-p sampling')
             parser.add_argument('-s', '--seed', type=int, default=-1, help='RNG seed')
-            parser.add_argument('--temp', type=float, default=0.8, help='temperature')
-            parser.add_argument('--repeat_penalty', type=float, default=1.3, help='penalize repeat sequence of tokens')
+            parser.add_argument('--temp', type=float, default=0.7, help='temperature')
+            parser.add_argument('--repeat_penalty', type=float, default=1.17647, help='penalize repeat sequence of tokens')
+            parser.add_argument('--repeat_last_n', type=int, default=256, help='last n tokens to consider for penalize')
             try:
                 args = parser.parse_args(shlex.split(message.content[7:], posix=True))
             except SystemExit:
@@ -39,8 +40,10 @@ async def on_message(message):
                 return
             model = './models/13B/ggml-model-q4_0.bin'
 
+            print(f'Generating text with args: {args}')
+
             # Build the command to execute
-            command = ['./main', '-m', model, '-t', str(args.threads), '-n', str(args.n_predict), '-c', str(args.ctx_size), '--temp', str(args.temp), '--top_k', str(args.top_k), '--top_p', str(args.top_p), '--repeat_penalty', str(args.repeat_penalty), '-s', str(args.seed), '-p', args.prompt]
+            command = ['./main', '-m', model, '-t', str(args.threads), '-n', str(args.n_predict), '-c', str(args.ctx_size), '--temp', str(args.temp), '--top_k', str(args.top_k), '--top_p', str(args.top_p), '--repeat_penalty', str(args.repeat_penalty), '-s', str(args.seed), '--repeat_last_n', str(args.repeat_last_n), '-p', args.prompt]
 
             # Send typing notification
             async with message.channel.typing():
@@ -49,8 +52,13 @@ async def on_message(message):
 
             # Send the output back to the channel
             if result.returncode == 0:
-                # truncate the output to 2000 characters
-                await message.channel.send(result.stdout[:2000])
+                if len(result.stdout) <= 2000:
+                    await message.channel.send(result.stdout)
+                else:
+                    # Upload the output as a file
+                    with open('llama-output.txt', 'w') as f:
+                        f.write(result.stdout)
+                    await message.channel.send(file=discord.File('llama-output.txt'))
             else:
                 await message.channel.send('An error occurred while running the command.')
         except Exception as e:
